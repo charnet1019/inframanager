3131bb88fc66a8c4e2bcecc52b2bd7bc3861f97d
root <root@slb.sunday.com>
2022-03-02 17:58:42 +0800

    更新readme

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 ## 主机信息管理系统
 
 ### 使用pipreqs生成requirements.txt
+
+### 初始化数据库
+```
+flask db init
+flask db migrate
+flask db upgrade
+```
+
+### 创建管理员用户
+```commandline
+flask createsuperuser
+```
--- app.py
+++ app.py
@@ -9,35 +9,36 @@ from common.exts import db
 
 app = create_app()
 
-# @app.cli.command()
-# @click.option('--username', default='admin', help='Administrator username')
-# @click.option('--password', default='Wimj@$1209', help='Administrator password')
-# @click.option('--fullname', default='administrator', help='Administrator full name')
-# @click.option('--is_active', default=True, help='is active by default')
-# @click.option('--is_admin', default=True, help='is administrator by default')
-# def createsuperuser(username, password, fullname='administrator', is_active=True, is_admin=True):
-#     # def createsuperuser(username, password):
-#     user = User.query.first()
-#     if user:
-#         click.echo('Updating user ...')
-#         user.username = username
-#         user.password = generate_password_hash(password)
-#         user.fullname = fullname
-#         user.is_active = is_active
-#         user.is_admin = is_admin
-#     else:
-#         click.echo('Add user ...')
-#         user = User()
-#         user.username = username
-#         user.password = generate_password_hash(password)
-#         print(user.password)
-#         user.fullname = fullname
-#         user.is_active = is_active
-#         user.is_admin = is_admin
-#         db.session.add(user)
-#
-#     db.session.commit()
-#     click.echo('Done.....')
+
+@app.cli.command()
+@click.option('--username', default='admin', help='Administrator username')
+@click.option('--password', default='Wimj@$1209', help='Administrator password')
+@click.option('--fullname', default='administrator', help='Administrator full name')
+@click.option('--is_active', default=True, help='is active by default')
+@click.option('--is_admin', default=True, help='is administrator by default')
+def createsuperuser(username, password, fullname='administrator', is_active=True, is_admin=True):
+    # def createsuperuser(username, password):
+    user = User.query.first()
+    if user:
+        click.echo('Updating user ...')
+        user.username = username
+        user.password = generate_password_hash(password)
+        user.fullname = fullname
+        user.is_active = is_active
+        user.is_admin = is_admin
+    else:
+        click.echo('Add user ...')
+        user = User()
+        user.username = username
+        user.password = generate_password_hash(password)
+        print(user.password)
+        user.fullname = fullname
+        user.is_active = is_active
+        user.is_admin = is_admin
+        db.session.add(user)
+
+    db.session.commit()
+    click.echo('Done.....')
 
 
 if __name__ == '__main__':
